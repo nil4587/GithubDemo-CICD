292b0697613231178f53dda367a18660818946ac
NileshKumar Prajapati <3828906+nil4587@users.noreply.github.com>
2023-03-26 14:11:07 +0200

    First commit

---
+++ GitHubDemo/GitHubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemoApp.swift
+//  GitHubDemo
+//
+//  Created by Nileshkumar M. Prajapati on 2023/03/26.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
