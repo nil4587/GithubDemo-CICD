5a4cd1ee2fb1da5c3156614a8403dd4aae752b32
NileshKumar Prajapati <3828906+nil4587@users.noreply.github.com>
2023-03-28 09:38:54 +0200

    Revert "Merge pull request #13 from nil4587/dev"
    This reverts commit d92ac9f804bb6595262bee41877c76f4ebda3f41, reversing
    changes made to 8d30281813229d252d7cb55e189854ac93de4e9d.

--- GitHubDemo/GitHubDemoApp.swift
+++ GitHubDemo/GitHubDemoApp.swift
@@ -22,7 +22,7 @@ struct GitHubDemoApp: App {
     }
 
     func launchMSAppCenter() {
-        AppCenter.start(withAppSecret: "940b8937-793d-4bad-bcac-81fdb14a1a55", services:[
+        AppCenter.start(withAppSecret: "adb3b1f3-e685-4949-8a0e-6921ae78d9b7", services:[
             Crashes.self, Analytics.self
         ])
     }
